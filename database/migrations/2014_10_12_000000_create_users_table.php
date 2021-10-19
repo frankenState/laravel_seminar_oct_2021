ec6ad2d09cb71b1113796ebba0e91f92e39ea915
Frank Lou <frank@nxt.work>
2021-10-19 16:05:11 +0800

    Configured the migration/table for users and feedback.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('username');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('contact_num');
+            $table->string('alt_contact_num');
+            $table->string('avatar');
+            $table->enum('type', ['ADMIN', 'USER']);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_10_19_075446_create_feedback_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('title');
+            $table->text('body');
+            $table->enum('status', ['PENDING','REVIEWED', 'CLOSED'])->default('PENDING');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
